6b88c56581004a31bd56bc4e2860ceffb0445f3d
Lidia Danish <lida@aaee.by>
2024-05-31 16:20:30 +0300

    Clear project

--- blocks/sections/hero/js/block-hero.js
+++ blocks/sections/hero/js/block-hero.js
@@ -5,21 +5,7 @@ import { initScript } from '../../../../resources/js/utils/init-script'
 
 
 const init = function() {
-	const $section = $( this )
-	const $scrollButton = $section.find( '.js-scroll-to-next-section' )
-	const $pageWrapper = $( '.wp-site-blocks' )
-	const $header = $( '.header' )
-
-	// Scroll Down Event
-	const scrollAction = event => {
-		const headerHeight = $header.innerHeight()
-		const sectionHeight = $section.innerHeight()
-		const scrollTop = headerHeight + sectionHeight
-
-		$pageWrapper.animate( { scrollTop }, 1000 )
-	}
-
-	$scrollButton.on( 'click', scrollAction )
+	const $section = $( this );
 }
 
 
